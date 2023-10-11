12b160d4d298eb07a5a78f100724500323320599
Ignatox <ignaciodiaz93@gmail.com>
2023-10-11 17:50:16 -0300

    Cambios, con errores a corregir clase 11/10

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
-//Global variables
-var allProducts = [];
-var itemsPerPage = 5;
-//Handlers
-function filterProducts(value) {
-    var filteredProducts = allProducts.filter(function (p) { return p.title.toLowerCase().includes(value.toLowerCase()) || p.description.toLowerCase().includes(value.toLowerCase()); });
-    console.log('filteredProducts', filteredProducts);
-}
-function sortProducts(prop) {
-    var sortedProducts = allProducts.toSorted(function (a, b) { return a[prop] > b[prop] ? 1 : a[prop] < b[prop] ? -1 : 0; });
-    console.log('sortedProducts');
-}
-function paginateProducts(page) {
-    var paginatedProducts = allProducts.slice(page * itemsPerPage, (page + 1) * 5); //Del 0 al 5, del 5 al 10, del 10 al 20...
-    console.log('paginatedProducts', paginatedProducts);
-}
-fetch('https://fakestoreapi.com/products')
-    .then(function (res) { return res.json(); })
-    .then(function (products) {
-    allProducts = products;
+// type Rating = {
+//   count: number;
+//   rate: number;
+// };
+// Global variables
+var displayedPeople = [];
+//Utils
+function drawTable(people) {
     // Prepare table HTML
-    var tableHTML = '<thead><tr><th>ID</th><th><button type="button" class="btn btn-link" onclick=sortProducts("title")>Title</button></th><th>Description</th><th>Price</th></tr></thead><tbody>';
-    // Loop thru all products to generate rows of the table
-    products.forEach(function (p) {
-        tableHTML += "<tr><td>".concat(p.id, "</td><td>").concat(p.title, "</td><td>").concat(p.description, "</td><td>").concat(p.price, "</td></tr>");
-    });
+    var tableHTML = '<thead><tr><th>name</th><th><button type="button" class="btn btn-link" onclick=sortPeople("birth_year")>BOD</button> </th><th>Gender</th><th>Price</th></tr></thead><tbody>';
+    //Loop thru all products to generate rows of the table
+    people.forEach(function (p) {
+        tableHTML += "<tr><td>".concat(p.name, "</td><td>").concat(p.birth_year, "</td><td>").concat(p.gender, "</td><td>").concat(p.url, "</td></tr>");
+    }); //Le paso las personas y pr cada persona dibuja una row e incrustalo en la tabla
     // Close table body
     tableHTML += '</tbody>';
     // Grab table element to set its inner HTML
     document.querySelector('#tableElement').innerHTML = tableHTML;
+}
+//Handlers
+function paginatePeople(page) {
+    fetch("https://swapi.dev/api/people/=page=%{page}")
+        // const paginatedProducts: Product[] = allProducts.slice(page * itemsPerPage, (page+1)*5 ); //Del 0 al 5, del 5 al 10, del 10 al 20...
+        .then(function (res) { return res.json(); })
+        .then(function (data) {
+        displayedPeople = data.results;
+        drawTable(data.results);
+    });
+}
+function filterPeople(value) {
+    var fileteredPeople = displayedPeople.filter(function (p) { return p.name.toLowerCase().includes(value.toLowerCase()) ||
+        p.birth_year.toLowerCase().includes(value.toLowerCase()) ||
+        p.url.toLowerCase().includes(value.toLowerCase()) ||
+        p.gender.toLowerCase().includes(value.toLowerCase()); });
+}
+fetch('https://swapi.dev/api/people')
+    .then(function (res) { return res.json(); })
+    .then(function (data) {
+    displayedPeople = data.results;
+    //We invoke the draw table
+    drawTable(data.results);
+    var pages = Math.ceil(data.count / 10); //redondeo la antidad de paginas con el contador
+    var paginationElement = document.querySelector('#paginationElement');
+    var pagesHTML = '';
+    for (var index = 1; index <= pages; index++) {
+        pagesHTML += "<li class=\"page-item\"><a class=\"page-link\" href=\"#\"".concat(index, " onclick=\"paginatePeople\"").concat(index, "</a></li>");
+    }
     // Hide spinner
     var spinnerElement = document.querySelector('#spinnerContainer');
     spinnerElement.style.display = 'none';
--- index.ts
+++ index.ts
@@ -1,50 +1,115 @@
-type Rating = {
+
+
+type Person = {
+
+    name: string,
+    height: number,
+    mass: number,
+    hair_color: string,
+    skin_color: string,
+    eye_color: string,
+    birth_year: string,
+    gender: string,
+    homeworld: string,
+    films: string[],
+    species: [],
+    vehicles: string[],
+    starships: string[],
+    created: Date,
+    edited: Date,
+    url: string
+}
+type ServerResponse = {
   count: number;
-  rate: number;
+  results: Person[];
 };
 
-type Product = {
-  category: string;
-  description: string;
-  id: number;
-  image: string;
-  price: number;
-  rating: Rating;
-  title: string;
-};
+// Global variables
+let displayedPeople: Person[] = [];
+let sortBy: any = {};
 
-//Global variables
-let allProducts: Product[] = [];
-const itemsPerPage: number=5;
-//Handlers
-function filterProducts(value: string){
-  const filteredProducts: Product[] = allProducts.filter((p: Product) => p.title.toLowerCase().includes(value.toLowerCase()) || p.description.toLowerCase().includes(value.toLowerCase())  )
-  console.log('filteredProducts', filteredProducts);
-}
-function sortProducts(prop:string){
-  const sortedProducts = allProducts.toSorted((a: Product, b: Product) => a[prop] > b[prop]? 1 : a[prop] < b[prop] ? -1 : 0  );
-  console.log('sortedProducts');
-}
-function paginateProducts(page: number){
-  const paginatedProducts: Product[] = allProducts.slice(page * itemsPerPage, (page+1)*5 ); //Del 0 al 5, del 5 al 10, del 10 al 20...
-  console.log('paginatedProducts', paginatedProducts);
-  
-}
 
-fetch('https://fakestoreapi.com/products')
-  .then(res => res.json())
-  .then((products: Product[]) => {
-    allProducts = products;
-    // Prepare table HTML
-    let tableHTML: string = '<thead><tr><th>ID</th><th><button type="button" class="btn btn-link" onclick=sortProducts("title")>Title</button></th><th>Description</th><th>Price</th></tr></thead><tbody>';
-    // Loop thru all products to generate rows of the table
-    products.forEach((p: Product) => {
-      tableHTML += `<tr><td>${p.id}</td><td>${p.title}</td><td>${p.description}</td><td>${p.price}</td></tr>`;
-    });
+
+
+//Utils
+function drawTable(people: Person[]){
+   // Prepare table HTML
+   let tableHTML: string = '<thead><tr><th>name</th><th><button type="button" class="btn btn-link" onclick=sortPeople("birth_year")>BOD</button> </th><th>Gender</th><th>Price</th></tr></thead><tbody>';
+   //Loop thru all products to generate rows of the table
+   people.forEach((p: Person) => {
+     tableHTML += `<tr><td>${p.name}</td><td>${p.birth_year}</td><td>${p.gender}</td><td>${p.url}</td></tr>`;
+   }); //Le paso las personas y pr cada persona dibuja una row e incrustalo en la tabla
     // Close table body
     tableHTML += '</tbody>';
     // Grab table element to set its inner HTML
     document.querySelector('#tableElement')!.innerHTML = tableHTML;
+}
+
+//Handlers
+
+function paginatePeople(page: number){
+  fetch(`https://swapi.dev/api/people/=page=%{page}`)
+    // const paginatedProducts: Product[] = allProducts.slice(page * itemsPerPage, (page+1)*5 ); //Del 0 al 5, del 5 al 10, del 10 al 20...
+    .then(res=> res.json())
+    .then((data:ServerResponse) =>{
+      displayedPeople = data.results; 
+      drawTable(data.results);
+    });
+
+}
+function filterPeople(value: string){
+  const filteredPeople = displayedPeople.filter((p: Person) => p.name.toLowerCase().includes(value.toLowerCase()) ||
+  p.birth_year.toLowerCase().includes(value.toLowerCase()) ||
+  p.url.toLowerCase().includes(value.toLowerCase()) ||
+  p.gender.toLowerCase().includes(value.toLowerCase())
+  );
+
+drawTable(filteredPeople);
+}
+function sortPeople(prop: string){
+  if (sortBy[prop]) {
+      if(sortBy[prop] === 'asc'){
+        sortBy[prop] = 'desc';
+      } else if(sortBy[prop] === 'desc'){
+        sortBy[prop] = null;
+      }
+  } else {
+    sortBy = {[prop]:  'asc' }
+
+    const sortedPeople = displayedPeople.toSorted((a:Person, b: Person)=> 
+     if (sortBy[prop] === 'asc'){
+      return a[prop] > b[prop] ? 1 :a[prop] < b[prop] ? -1 : 0 ;
+
+    } else if (sortBy[prop] === 'desc'){
+      return a[prop] < b[prop] ? 1 :a[prop] > b[prop] ? -1 : 0 ;
+
+    } else {
+      return displayedPeople;
+    }
+    );
+  
+}
+}
+
+
+fetch('https://swapi.dev/api/people')
+  .then(res => res.json())
+  .then((data:ServerResponse) => {
+
+    displayedPeople = data.results;
+   
+   //We invoke the draw table
+   drawTable(data.results);
+    const pages = Math.ceil(data.count / 10); //redondeo la antidad de paginas con el contador
+    const paginationElement = document.querySelector('#paginationElement');
+    let pagesHTML = '';
+
+    for (let index = 1; index <= pages; index++) {
+    
+      pagesHTML += `<li class="page-item"><a class="page-link" href="#"${index} onclick="paginatePeople"${index}</a></li>`
+      
+    }
+
     // Hide spinner
     const spinnerElement: HTMLElement = document.querySelector('#spinnerContainer')!;
     spinnerElement.style.display = 'none';
